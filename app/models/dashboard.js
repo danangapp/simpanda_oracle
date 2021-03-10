9c6c2364fffc4cb6ad2ab8391bfe17c99810c39b
Danang Rusmanto <danang@roots.co.id>
2021-03-10 20:02:04 +0700

    update dashboard

--- app/models/dashboard.js
+++ app/models/dashboard.js
@@ -9,7 +9,7 @@ Dashboard.statisticresume = async (param, result) => {
     var queryText = `SELECT max(b."id") as "id", max(b."nama") as "nama", COUNT( a."cabang_id" ) AS "total" FROM "asset_kapal" a INNER JOIN "cabang" b ON a."cabang_id" = b."id" WHERE a."enable" = 1 AND a."approval_status_id" = 1 AND a."tipe_asset_id" = 2 GROUP BY a."cabang_id"`;
     const exec = f.query(queryText);
     const res = await exec;
-    result(null, res);
+    result(null, res.rows);
 };
 
 
@@ -17,7 +17,7 @@ Dashboard.cetivicatevalidity = async (param, result) => {
     var queryText = `SELECT "tanggal_expire", "personil_id", "personil"."tipe_personil_id", "sertifikat"."asset_kapal_id" FROM "sertifikat" LEFT JOIN "personil" ON "sertifikat"."personil_id" = "personil"."id" LEFT JOIN "asset_kapal" ON "sertifikat"."asset_kapal_id" = "asset_kapal"."id" WHERE "tanggal_expire" > SYSDATE`;
     const exec = f.query(queryText);
     const res = await exec;
-    result(null, res);
+    result(null, res.rows);
 };
 
 module.exports = Dashboard;
