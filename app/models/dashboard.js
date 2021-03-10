64cfc75cc488c30b4563fd42a8112ed18bdc72c5
Danang Rusmanto <danang@roots.co.id>
2021-03-10 19:58:17 +0700

    update dashboard

--- app/models/dashboard.js
+++ app/models/dashboard.js
@@ -1,8 +1,4 @@
-const sql = require("../config/db.js");
-const util = require('util');
-const query = util.promisify(sql.query).bind(sql);
 const f = require('../controllers/function');
-var objek = new Object();
 
 // constructor
 const Dashboard = function (dashboard) {
@@ -10,30 +6,18 @@ const Dashboard = function (dashboard) {
 };
 
 Dashboard.statisticresume = (param, result) => {
-    var query = "SELECT cabang.id, cabang.nama, COUNT(cabang_id) as total FROM asset_kapal JOIN cabang ON asset_kapal.cabang_id = cabang.id WHERE enable = 1 AND approval_status_id = 1 AND tipe_asset_id = 2 GROUP BY cabang_id";
-    sql.query(query, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-
-        result(null, res);
-    });
+    var queryText = `SELECT max(b."id") as "id", max(b."nama") as "nama", COUNT( a."cabang_id" ) AS "total" FROM "asset_kapal" a INNER JOIN "cabang" b ON a."cabang_id" = b."id" WHERE a."enable" = 1 AND a."approval_status_id" = 1 AND a."tipe_asset_id" = 2 GROUP BY a."cabang_id"`;
+    const exec = f.query(queryText);
+    const res = await exec;
+    result(null, res);
 };
 
 
 Dashboard.cetivicatevalidity = (param, result) => {
-    var query = "SELECT tanggal_expire, personil_id, personil.tipe_personil_id, sertifikat.asset_kapal_id FROM sertifikat LEFT JOIN personil ON sertifikat.personil_id = personil.id LEFT JOIN asset_kapal ON sertifikat.asset_kapal_id = asset_kapal.id WHERE tanggal_expire > CURRENT_DATE ()";
-    sql.query(query, (err, res) => {
-        if (err) {
-            console.log("error: ", err);
-            result(null, err);
-            return;
-        }
-
-        result(null, res);
-    });
+    var queryText = `SELECT "tanggal_expire", "personil_id", "personil"."tipe_personil_id", "sertifikat"."asset_kapal_id" FROM "sertifikat" LEFT JOIN "personil" ON "sertifikat"."personil_id" = "personil"."id" LEFT JOIN "asset_kapal" ON "sertifikat"."asset_kapal_id" = "asset_kapal"."id" WHERE "tanggal_expire" > SYSDATE`;
+    const exec = f.query(queryText);
+    const res = await exec;
+    result(null, res);
 };
 
 module.exports = Dashboard;
