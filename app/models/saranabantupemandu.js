c1bbc1691005a25a86d64e00ec195eebdf60be13
Danang Rusmanto <danang@roots.co.id>
2021-03-29 14:45:28 +0700

    update all

--- app/models/saranabantupemandu.js
+++ app/models/saranabantupemandu.js
@@ -3,35 +3,35 @@ var objek = new Object();
 
 // constructor
 const SaranaBantuPemandu = function (saranabantupemandu) {
-    this.approval_status_id = saranabantupemandu.approval_status_id;
-    this.cabang_id = saranabantupemandu.cabang_id;
-    this.tanggal_pemeriksaan = saranabantupemandu.tanggal_pemeriksaan;
-    this.pelaksana = saranabantupemandu.pelaksana;
-    this.nama = saranabantupemandu.nama;
-    this.tipe_asset_id = saranabantupemandu.tipe_asset_id;
-    this.jabatan = saranabantupemandu.jabatan;
-    this.asset_kapal_id = saranabantupemandu.asset_kapal_id;
-    this.status_ijazah_id = saranabantupemandu.status_ijazah_id;
-    this.sarana_bantu_pemandu_personil = saranabantupemandu.sarana_bantu_pemandu_personil;
+	this.approval_status_id = saranabantupemandu.approval_status_id;
+	this.cabang_id = saranabantupemandu.cabang_id;
+	this.tanggal_pemeriksaan = saranabantupemandu.tanggal_pemeriksaan;
+	this.pelaksana = saranabantupemandu.pelaksana;
+	this.nama = saranabantupemandu.nama;
+	this.tipe_asset_id = saranabantupemandu.tipe_asset_id;
+	this.jabatan = saranabantupemandu.jabatan;
+	this.asset_kapal_id = saranabantupemandu.asset_kapal_id;
+	this.status_ijazah_id = saranabantupemandu.status_ijazah_id;
+	this.sarana_bantu_pemandu_personil = saranabantupemandu.sarana_bantu_pemandu_personil;
 };
 
 const setActivity = (objects, koneksi = 1) => {
-		objek.date = f.toDate(objects.date);
-		objek.item = 'saranabantupemandu';
-		objek.action = objects.approval_status_id;
-		objek.user_id = objects.user_id;
-		objek.remark = objects.remark;
-		objek.koneksi = koneksi;
-		delete objects.date;
-		delete objects.item;
-		delete objects.action;
-		delete objects.user_id;
-		delete objects.remark;
-		delete objects.koneksi;
-		return objects
+	objek.date = f.toDate(objects.date);
+	objek.item = 'saranabantupemandu';
+	objek.action = objects.approval_status_id;
+	objek.user_id = objects.user_id;
+	objek.remark = objects.remark;
+	objek.koneksi = koneksi;
+	delete objects.date;
+	delete objects.item;
+	delete objects.action;
+	delete objects.user_id;
+	delete objects.remark;
+	delete objects.koneksi;
+	return objects
 };
 
-SaranaBantuPemandu.create = async(newSaranaBantuPemandu, result, cabang_id, user_id) => {
+SaranaBantuPemandu.create = async (newSaranaBantuPemandu, result, cabang_id, user_id) => {
 	const question = newSaranaBantuPemandu.question;
 	delete newSaranaBantuPemandu.question;
 	newSaranaBantuPemandu = setActivity(newSaranaBantuPemandu);
@@ -44,14 +44,14 @@ SaranaBantuPemandu.create = async(newSaranaBantuPemandu, result, cabang_id, user
 
 	for (var i in question) {
 		var id_sbpdata = await f.getid("sbp_data");
-	    const x = question[i];
+		const x = question[i];
 		x['sarana_bantu_pemandu_id'] = id;
 		x['answer'] = x['value'];
 		delete x.value;
-	
-	    var header = "", value = "";
-	    value = value.substring(0, value.length - 2);
-	    header = header.substring(0, header.length - 2);
+
+		var header = "", value = "";
+		value = value.substring(0, value.length - 2);
+		header = header.substring(0, header.length - 2);
 		const hv = await f.headerValue(x, id_sbpdata);
 		await f.query("INSERT INTO \"sbp_data\"" + hv);
 	}
@@ -78,16 +78,16 @@ SaranaBantuPemandu.findById = async (id, result) => {
 	}
 	const question = { "question": resQuery.rows }
 	const activityLog = { "activityLog": resActivityLog.rows }
-	let merge = { ...res.rows[0], ...question, ...activityLog }	
+	let merge = { ...res.rows[0], ...question, ...activityLog }
 	result(null, merge);
 }
 
 SaranaBantuPemandu.getAll = async (param, result, cabang_id) => {
-    var wheres = f.getParam(param, "sarana_bantu_pemandu");
-    var query = "SELECT a.* , a1.\"nama\" as \"approval_status\", a2.\"nama\" as \"cabang\", a3.\"nama\" as \"tipe_asset\", a4.\"nama_asset\" as \"asset_kapal\", a5.\"nama\" as \"status_ijazah\" , a3.\"sarana_config_question\" FROM \"sarana_bantu_pemandu\" a  LEFT JOIN \"approval_status\" a1 ON a.\"approval_status_id\" = a1.\"id\"  LEFT JOIN \"cabang\" a2 ON a.\"cabang_id\" = a2.\"id\"  LEFT JOIN \"tipe_asset\" a3 ON a.\"tipe_asset_id\" = a3.\"id\"  LEFT JOIN \"asset_kapal\" a4 ON a.\"asset_kapal_id\" = a4.\"id\"  LEFT JOIN \"status_ijazah\" a5 ON a.\"status_ijazah_id\" = a5.\"id\" ";
+	var wheres = f.getParam(param, "sarana_bantu_pemandu");
+	var query = "SELECT a.* , a1.\"nama\" as \"approval_status\", a2.\"nama\" as \"cabang\", a3.\"nama\" as \"tipe_asset\", a4.\"nama_asset\" as \"asset_kapal\", a5.\"nama\" as \"status_ijazah\" , a3.\"sarana_config_question\" FROM \"sarana_bantu_pemandu\" a  LEFT JOIN \"approval_status\" a1 ON a.\"approval_status_id\" = a1.\"id\"  LEFT JOIN \"cabang\" a2 ON a.\"cabang_id\" = a2.\"id\"  LEFT JOIN \"tipe_asset\" a3 ON a.\"tipe_asset_id\" = a3.\"id\"  LEFT JOIN \"asset_kapal\" a4 ON a.\"asset_kapal_id\" = a4.\"id\"  LEFT JOIN \"status_ijazah\" a5 ON a.\"status_ijazah_id\" = a5.\"id\" ";
 	if (param.q) {
 		wheres += wheres.length == 7 ? "(" : "AND (";
-		wheres += "a.\"approval_status_id\" LIKE '%" + param.q + "%' OR a.\"cabang_id\" LIKE '%" + param.q + "%' OR a.\"tanggal_pemeriksaan\" LIKE '%" + param.q + "%' OR a.\"pelaksana\" LIKE '%" + param.q + "%' OR a.\"nama\" LIKE '%" + param.q + "%' OR a.\"tipe_asset_id\" LIKE '%" + param.q + "%' OR a.\"jabatan\" LIKE '%" + param.q + "%' OR a.\"asset_kapal_id\" LIKE '%" + param.q + "%' OR a.\"status_ijazah_id\" LIKE '%" + param.q + "%' OR a.\"sarana_bantu_pemandu_personil\" LIKE '%" + param.q + "%'";	
+		wheres += "a.\"approval_status_id\" LIKE '%" + param.q + "%' OR a.\"cabang_id\" LIKE '%" + param.q + "%' OR a.\"tanggal_pemeriksaan\" LIKE '%" + param.q + "%' OR a.\"pelaksana\" LIKE '%" + param.q + "%' OR a.\"nama\" LIKE '%" + param.q + "%' OR a.\"tipe_asset_id\" LIKE '%" + param.q + "%' OR a.\"jabatan\" LIKE '%" + param.q + "%' OR a.\"asset_kapal_id\" LIKE '%" + param.q + "%' OR a.\"status_ijazah_id\" LIKE '%" + param.q + "%' OR a.\"sarana_bantu_pemandu_personil\" LIKE '%" + param.q + "%'";
 		wheres += ")";
 	}
 
@@ -99,20 +99,20 @@ SaranaBantuPemandu.getAll = async (param, result, cabang_id) => {
 	result(null, res.rows);
 }
 
-SaranaBantuPemandu.updateById = async(id, saranabantupemandu, result, user_id) => {
+SaranaBantuPemandu.updateById = async (id, saranabantupemandu, result, user_id) => {
 
 	const question = saranabantupemandu.question;
-	await f.query("DELETE FROM \"sbp_data\" WHERE \"sarana_bantu_pemandu_id\" = '\" + id + \"' ");
+	await f.query("DELETE FROM \"sbp_data\" WHERE \"sarana_bantu_pemandu_id\" = '" + id + "' ");
 	for (var i in question) {
 		var id_sbpdata = await f.getid("sbp_data");
-	    const x = question[i];
+		const x = question[i];
 		x['sarana_bantu_pemandu_id'] = id;
 		x['answer'] = x['value'];
 		delete x.value;
-	
-	    var header = "", value = "";
-	    value = value.substring(0, value.length - 2);
-	    header = header.substring(0, header.length - 2);
+
+		var header = "", value = "";
+		value = value.substring(0, value.length - 2);
+		header = header.substring(0, header.length - 2);
 		const hv = await f.headerValue(x, id_sbpdata);
 		await f.query("INSERT INTO \"sbp_data\"" + hv);
 	}
