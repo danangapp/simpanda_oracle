c3336ef0dfb14ef4284ce5e0bb6a49137087fec3
Danang Rusmanto <danang@roots.co.id>
2021-04-20 15:03:26 +0700

    updaet dashboard

--- app/models/personil.js
+++ app/models/personil.js
@@ -259,8 +259,10 @@ Personil.updateById = async (id, personil, result, user_id) => {
 	var str = f.getValueUpdate(personil, id, arr);
 	await f.approvalStatus("personil", personil, objek, id, user_id)
 	if (personil.is_from_simop) {
+		console.log("UPDATE \"personil\" SET " + str + " WHERE \"simop_kd_pers_pandu\" = '" + personil.simop_kd_pers_pandu + "'");
 		await f.query("UPDATE \"personil\" SET " + str + " WHERE \"simop_kd_pers_pandu\" = '" + personil.simop_kd_pers_pandu + "'", 2);
 	} else {
+		console.log("UPDATE \"personil\" SET " + str + " WHERE \"id\" = '" + id + "'");
 		await f.query("UPDATE \"personil\" SET " + str + " WHERE \"id\" = '" + id + "'", 2);
 	}
 
