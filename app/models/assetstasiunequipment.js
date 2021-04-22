89fcd571ded5052a581c61b4202acddbdc533031
Danang Rusmanto <danang@roots.co.id>
2021-04-22 15:52:55 +0700

    update all

--- app/models/assetstasiunequipment.js
+++ app/models/assetstasiunequipment.js
@@ -3,36 +3,36 @@ var objek = new Object();
 
 // constructor
 const AssetStasiunEquipment = function (assetstasiunequipment) {
-    this.nomor_asset = assetstasiunequipment.nomor_asset;
-    this.tipe_asset_id = assetstasiunequipment.tipe_asset_id;
-    this.nama = assetstasiunequipment.nama;
-    this.tahun_perolehan = assetstasiunequipment.tahun_perolehan;
-    this.nilai_perolehan = assetstasiunequipment.nilai_perolehan;
-    this.kondisi = assetstasiunequipment.kondisi;
-    this.approval_status_id = assetstasiunequipment.approval_status_id;
-    this.enable = assetstasiunequipment.enable;
-    this.cabang_id = assetstasiunequipment.cabang_id;
+	this.nomor_asset = assetstasiunequipment.nomor_asset;
+	this.tipe_asset_id = assetstasiunequipment.tipe_asset_id;
+	this.nama = assetstasiunequipment.nama;
+	this.tahun_perolehan = assetstasiunequipment.tahun_perolehan;
+	this.nilai_perolehan = assetstasiunequipment.nilai_perolehan;
+	this.kondisi = assetstasiunequipment.kondisi;
+	this.approval_status_id = assetstasiunequipment.approval_status_id;
+	this.enable = assetstasiunequipment.enable;
+	this.cabang_id = assetstasiunequipment.cabang_id;
 };
 
 const setActivity = (objects, koneksi = 1) => {
-		objek.date = f.toDate(objects.date);
-		objek.item = 'assetstasiunequipment';
-		objek.action = objects.approval_status_id;
-		objek.user_id = objects.user_id;
-		objek.remark = objects.remark;
-		objek.koneksi = koneksi;
-		objek.keterangan = objects.keterangan;
-		delete objects.date;
-		delete objects.item;
-		delete objects.action;
-		delete objects.user_id;
-		delete objects.remark;
-		delete objects.koneksi;
-		delete objects.keterangan;
-		return objects
+	objek.date = f.toDate(objects.date);
+	objek.item = 'assetstasiunequipment';
+	objek.action = objects.approval_status_id;
+	objek.user_id = objects.user_id;
+	objek.remark = objects.remark;
+	objek.koneksi = koneksi;
+	objek.keterangan = objects.keterangan;
+	delete objects.date;
+	delete objects.item;
+	delete objects.action;
+	delete objects.user_id;
+	delete objects.remark;
+	delete objects.koneksi;
+	delete objects.keterangan;
+	return objects
 };
 
-AssetStasiunEquipment.create = async(newAssetStasiunEquipment, result, cabang_id, user_id) => {
+AssetStasiunEquipment.create = async (newAssetStasiunEquipment, result, cabang_id, user_id) => {
 	newAssetStasiunEquipment = setActivity(newAssetStasiunEquipment);
 	var id = await f.getid("asset_stasiun_equipment");
 	const hv = await f.headerValue(newAssetStasiunEquipment, id);
@@ -53,22 +53,22 @@ AssetStasiunEquipment.create = async(newAssetStasiunEquipment, result, cabang_id
 };
 
 AssetStasiunEquipment.findById = async (id, result) => {
-	const resActivityLog = await f.query("SELECT a.\"date\", a.\"item\", a.\"action\", a.\"user_id\", a.\"remark\", a.\"keterangan\", a.\"koneksi\" FROM \"activity_log\" a INNER JOIN \"asset_stasiun_equipment\" b ON a.\"item\" = 'asset_stasiun_equipment' AND a.\"koneksi\" = b.\"id\" WHERE b.\"id\" =  '" + id + "'");
+	const resActivityLog = await f.query("SELECT a.\"date\", a.\"item\", a.\"action\", a.\"user_id\", a.\"remark\", a.\"keterangan\", a.\"koneksi\" FROM \"activity_log\" a INNER JOIN \"asset_stasiun_equipment\" b ON a.\"item\" = 'assetstasiunequipment' AND a.\"koneksi\" = b.\"id\" WHERE b.\"id\" =  '" + id + "'");
 	var queryText = "SELECT a.* , a1.\"flag\" as \"tipe_asset\", a2.\"nama\" as \"approval_status\", a3.\"nama\" as \"ena\", a4.\"nama\" as \"cabang\" FROM \"asset_stasiun_equipment\" a  LEFT JOIN \"tipe_asset\" a1 ON a.\"tipe_asset_id\" = a1.\"id\"  LEFT JOIN \"approval_status\" a2 ON a.\"approval_status_id\" = a2.\"id\"  LEFT JOIN \"enable\" a3 ON a.\"enable\" = a3.\"id\"  LEFT JOIN \"cabang\" a4 ON a.\"cabang_id\" = a4.\"id\"   WHERE a.\"id\" = '" + id + "'";
 	const exec = f.query(queryText);
 	const res = await exec;
 	const activityLog = { "activityLog": resActivityLog.rows }
-	let merge = { ...res.rows[0], ...activityLog }	
+	let merge = { ...res.rows[0], ...activityLog }
 	result(null, merge);
 }
 
 AssetStasiunEquipment.getAll = async (param, result, cabang_id) => {
-    var wheres = f.getParam(param, "asset_stasiun_equipment");
+	var wheres = f.getParam(param, "asset_stasiun_equipment");
 	wheres = wheres.replace(`a."flag"`, `a1."flag"`);
-    var query = "SELECT a.* , a1.\"flag\" as \"tipe_asset\", a2.\"nama\" as \"approval_status\", a3.\"nama\" as \"ena\", a4.\"nama\" as \"cabang\" FROM \"asset_stasiun_equipment\" a  LEFT JOIN \"tipe_asset\" a1 ON a.\"tipe_asset_id\" = a1.\"id\"  LEFT JOIN \"approval_status\" a2 ON a.\"approval_status_id\" = a2.\"id\"  LEFT JOIN \"enable\" a3 ON a.\"enable\" = a3.\"id\"  LEFT JOIN \"cabang\" a4 ON a.\"cabang_id\" = a4.\"id\" ";
+	var query = "SELECT a.* , a1.\"flag\" as \"tipe_asset\", a2.\"nama\" as \"approval_status\", a3.\"nama\" as \"ena\", a4.\"nama\" as \"cabang\" FROM \"asset_stasiun_equipment\" a  LEFT JOIN \"tipe_asset\" a1 ON a.\"tipe_asset_id\" = a1.\"id\"  LEFT JOIN \"approval_status\" a2 ON a.\"approval_status_id\" = a2.\"id\"  LEFT JOIN \"enable\" a3 ON a.\"enable\" = a3.\"id\"  LEFT JOIN \"cabang\" a4 ON a.\"cabang_id\" = a4.\"id\" ";
 	if (param.q) {
 		wheres += wheres.length == 7 ? "(" : "AND (";
-		wheres += "a.\"nomor_asset\" LIKE '%" + param.q + "%' OR a.\"tipe_asset_id\" LIKE '%" + param.q + "%' OR a.\"nama\" LIKE '%" + param.q + "%' OR a.\"tahun_perolehan\" LIKE '%" + param.q + "%' OR a.\"nilai_perolehan\" LIKE '%" + param.q + "%' OR a.\"kondisi\" LIKE '%" + param.q + "%' OR a.\"approval_status_id\" LIKE '%" + param.q + "%' OR a.\"enable\" LIKE '%" + param.q + "%' OR a.\"cabang_id\" LIKE '%" + param.q + "%'";	
+		wheres += "a.\"nomor_asset\" LIKE '%" + param.q + "%' OR a.\"tipe_asset_id\" LIKE '%" + param.q + "%' OR a.\"nama\" LIKE '%" + param.q + "%' OR a.\"tahun_perolehan\" LIKE '%" + param.q + "%' OR a.\"nilai_perolehan\" LIKE '%" + param.q + "%' OR a.\"kondisi\" LIKE '%" + param.q + "%' OR a.\"approval_status_id\" LIKE '%" + param.q + "%' OR a.\"enable\" LIKE '%" + param.q + "%' OR a.\"cabang_id\" LIKE '%" + param.q + "%'";
 		wheres += ")";
 	}
 
@@ -80,25 +80,20 @@ AssetStasiunEquipment.getAll = async (param, result, cabang_id) => {
 	result(null, res.rows);
 }
 
-AssetStasiunEquipment.updateById = async(id, assetstasiunequipment, result, user_id) => {
+AssetStasiunEquipment.updateById = async (id, assetstasiunequipment, result, user_id) => {
 
 	var arr = ["nomor_asset", "tipe_asset_id", "nama", "tahun_perolehan", "nilai_perolehan", "kondisi", "approval_status_id", "enable", "cabang_id"];
 	var str = f.getValueUpdate(assetstasiunequipment, id, arr);
-	var id_activity_log = await f.getid("activity_log");
-	objek.koneksi = id;
-	objek.action = assetstasiunequipment.approval_status_id;
-	objek.keterangan = assetstasiunequipment.keterangan;
-	objek.item = "assetstasiunequipment";
-	objek.user_id = user_id;
-	if(assetstasiunequipment.approval_status_id == 1){
-		objek.remark = "Pengajuan disetujui oleh pusat";
-	}else if(assetstasiunequipment.approval_status_id == 2){
-		objek.remark = "Pengajuan ditolak oleh pusat";
-	}else if(assetstasiunequipment.approval_status_id == 0){
-		objek.remark = "Pengajuan dirubah oleh admin cabang";
+
+	if (assetstasiunequipment.approval_status_id > 0) {
+		objek.koneksi = id;
+		objek.action = assetstasiunequipment.approval_status_id;
+		objek.keterangan = assetstasiunequipment.keterangan;
+		objek.item = "assetstasiunequipment";
+		objek.user_id = user_id;
+		await f.approvalStatus("assetstasiunequipment", assetstasiunequipment, objek, id, user_id)
 	}
-	const hval = await f.headerValue(objek, id_activity_log);
-	await f.query("INSERT INTO \"activity_log\" " + hval, 2);
+
 	await f.query("UPDATE \"asset_stasiun_equipment\" SET " + str + " WHERE \"id\" = '" + id + "'", 2);
 	result(null, { id: id, ...assetstasiunequipment });
 };
