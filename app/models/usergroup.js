e9cc3a92130234174f097076859121ba0d71b6ae
Danang Rusmanto <danang@roots.co.id>
2021-04-27 22:20:49 +0700

    update date desc

--- app/models/usergroup.js
+++ app/models/usergroup.js
@@ -59,7 +59,7 @@ UserGroup.getAll = async (param, result, cabang_id) => {
 
 	wheres += f.whereCabang(cabang_id, `a."cabang_id"`, wheres.length);
 	query += wheres;
-	query += "ORDER BY a.\"upd_date\" DESC";
+	query += `ORDER BY a."upd_date", a."id" DESC`;
 	const exec = f.query(query);
 	const res = await exec;
 	result(null, res.rows);
