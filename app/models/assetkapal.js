6a13c2c1aec33429e688287297a12badaf5bdac5
irfanzakki <irfan_zakki@ymail.com>
2021-05-07 00:45:27 +0700

    up

--- app/models/assetkapal.js
+++ app/models/assetkapal.js
@@ -89,6 +89,20 @@ AssetKapal.create = async (newAssetKapal, result, cabang_id, user_id) => {
 	delete newAssetKapal.activity_keterangan;
 	let valid = newAssetKapal
 
+	var check = `SELECT "simop_kd_fas","nama_asset" FROM "asset_kapal"
+					WHERE LOWER("simop_kd_fas") like LOWER('%${newAssetKapal.kd_fas}%')
+					OR LOWER("nama_asset") like LOWER('%${newAssetKapal.nama_asset}%')`
+	var dataCheck = await f.query(check)
+	if (dataCheck.rows.length > 0) {
+		if (dataCheck.rows[0].simop_kd_fas.toLowerCase() === newAssetKapal.kd_fas.toLowerCase()) {
+			result(null, { 'status': false, 'message': 'Maaf, Kode fasilitas yang anda pilih sudah tersedia !' })
+		}else if(dataCheck.rows[0].nama_asset.toLowerCase() === newAssetKapal.nama_asset.toLowerCase()){
+			result(null, { 'status': false, 'message': 'Maaf, Nama kapal yang anda pilih sudah tersedia !' })
+		}
+
+		return false
+	}
+
 
 	// newAssetKapal.simop_kd_fas = newAssetKapal.kd_fas;
 	// delete newAssetKapal.kd_fas;
@@ -172,6 +186,19 @@ AssetKapal.getAll = async (param, result, cabang_id) => {
 
 AssetKapal.updateById = async (id, assetkapal, result, user_id, cabang_id) => {
 	// console.log("assetkapalnya ya", assetkapal);
+	var check = `SELECT "simop_kd_fas","nama_asset" FROM "asset_kapal"
+					WHERE LOWER("simop_kd_fas") like LOWER('%${assetkapal.kd_fas}%')
+					OR LOWER("nama_asset") like LOWER('%${assetkapal.nama_asset}%') AND "id" != '${id}'`
+	var dataCheck = await f.query(check)
+	if (dataCheck.rows.length > 0) {
+		if (dataCheck.rows[0].simop_kd_fas.toLowerCase() === assetkapal.kd_fas.toLowerCase()) {
+			result(null, { 'status': false, 'message': 'Maaf, Kode fasilitas yang anda pilih sudah tersedia !' })
+		}else if(dataCheck.rows[0].nama_asset.toLowerCase() === assetkapal.nama_asset.toLowerCase()){
+			result(null, { 'status': false, 'message': 'Maaf, Nama kapal yang anda pilih sudah tersedia !' })
+		}
+		return false
+	}
+
 	assetkapal.simop_kd_fas = assetkapal.kd_fas;
 	const sertifikat = assetkapal.sertifikat;
 	if (assetkapal.sertifikat) {
