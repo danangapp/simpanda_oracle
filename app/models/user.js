93fac754209aa62c8e7df47a216ca5b9dd64b190
Danang Rusmanto <danang@roots.co.id>
2021-05-24 16:25:27 +0700

    update all

--- app/models/user.js
+++ app/models/user.js
@@ -13,7 +13,7 @@ const User = function (user) {
 };
 
 const checkPassword = (str) => {
-	var re = /^(?=.*\d)(?=.*[a-z]).{8,}$/;
+	var re = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
 	return re.test(str);
 }
 
