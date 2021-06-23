e7572256978b919c269da689aff3d91ce3ae1b69
heryfidiawan07 <heryfidiawan07@gmail.com>
2021-06-23 11:06:52 +0700

    fix bug button terima pusat

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -1,6 +1,6 @@
 module.exports = {
-    user: "SIMPANDA",
-    password: "5impand4",
-    connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.88.44.80)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=SIMPROD)))",
+    user: "simpanda",
+    password: "simpanda",
+    connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.88.49.28)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=simpdev)))",
     externalAuth: false
 };
