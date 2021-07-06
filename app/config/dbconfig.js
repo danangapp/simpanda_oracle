71da4e116b2a4f2c5a2dc8a36d0c00d1b41f68ee
pryamm <pryamm@gmail.com>
2021-07-06 12:41:31 +0700

    config

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 module.exports = {
-    user: "simpanda",
-    password: "simpanda",
-    connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=10.88.49.28)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=simpdev)))",
+    user: process.env.USERS,
+    password: process.env.PASSWORD,
+    connectString: `(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=${process.env.HOSTS})(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=${process.env.SERVICE_NAME})))`,
     externalAuth: false
 };
