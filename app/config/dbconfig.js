ae12934be9ab1930ca7390236f22fc8fda017136
pryamm <pryamm@gmail.com>
2021-07-13 12:49:51 +0700

    fix

--- app/config/dbconfig.js
+++ app/config/dbconfig.js
@@ -2,6 +2,6 @@ require('dotenv').config();
 module.exports = {
     user: process.env.USERS,
     password: process.env.PASSWORD,
-    connectString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST="+process.env.HOSTS+")(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME="+process.env.SERVICE_NAME+")))",
+    connectString: `(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=${process.env.HOSTS})(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=${process.env.SERVICE_NAME})))`,
     externalAuth: false
 };
